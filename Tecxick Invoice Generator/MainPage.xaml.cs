1f109ef8652f1caa9524d202288805cfd2662c2d
Pasan Anuththara <pasananuththara19@gmail.com>
2022-10-09 09:45:08 +0530

    Event Logger Created

--- Tecxick Invoice Generator/MainPage.xaml.cs
+++ Tecxick Invoice Generator/MainPage.xaml.cs
@@ -25,6 +25,27 @@ namespace Tecxick_Invoice_Generator
         public MainPage()
         {
             this.InitializeComponent();
+            NavigateToStartScreen(this);
+        }
+
+        /// <summary>
+        /// Navigate the Main Screen to Start Screen
+        /// </summary>
+        /// <param name="RequestedFrom">From Which page this is requesting from</param>
+        public async void NavigateToStartScreen(object RequestedFrom)
+        {
+            await Model.General.Log("Navigation requested <" + (RequestedFrom as Page).ToString() + "><" + typeof(StartScreen).ToString() + ">" );
+            PageLoader.Navigate(typeof(StartScreen));
+        }
+
+        private async void PageLoader_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            await Model.General.Log("Navigation Failed" + e.Exception.ToString());
+        }
+
+        private async void PageLoader_Navigated(object sender, NavigationEventArgs e)
+        {
+            await Model.General.Log("Navigation success > " + e.SourcePageType.ToString());
         }
     }
 }
---
+++ Tecxick Invoice Generator/Model/General.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecxick_Invoice_Generator.Model
+{
+    internal class General
+    {
+        /// <summary>
+        /// Writes a Message to the Log File
+        /// </summary>
+        /// <param name="Message">Message That Needs to be written in the Log</param>
+        internal static async Task Log(String Message)
+        {
+            // Get the Application Local Folder
+            var AppFolder = Windows.Storage.ApplicationData.Current.LocalFolder;
+            // Create the Log file and if it is already exixsts, open it for the operation
+            var LogFile = await AppFolder.CreateFileAsync("Log.txt", Windows.Storage.CreationCollisionOption.OpenIfExists);
+            // When the Write Request is called
+            string Timestamp = "[" + DateTime.Now.ToString("G") + "] ";
+            // Build the full Text and add a line breake
+            string textToWrite = Timestamp + Message + "\n";
+            // Write the Text to the Log File
+            await Windows.Storage.FileIO.AppendTextAsync(LogFile, textToWrite);
+
+            //Debug
+            Debug.WriteLine(LogFile.Path);
+            
+        }
+    }
+}
